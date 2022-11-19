c6a2b35d9c8bc5ef91720a8fb94f16fb279059d6
erebuseternal <marcelsanders96@gmail.com>
2022-11-19 14:50:41 -0500

    image 2

--- buggy_webapp/flask/main/views.py
+++ buggy_webapp/flask/main/views.py
@@ -1,6 +1,6 @@
 import requests
 
-from flask import render_template, redirect, url_for, current_app
+from flask import render_template, redirect, url_for, current_app, make_response
 from flask_login import login_required, current_user
 from . import main
 
@@ -11,7 +11,7 @@ def home():
 @main.route('/submissions', methods=['GET'])
 @login_required
 def submissions_view():
-    data = {
+    payload = {
         'kobo_username': current_app.config['KOBO_USERNAME'],
         'kobo_password': current_app.config['KOBO_PASSWORD'],
         'kobo_uid': current_app.config['KOBO_UID'],
@@ -22,11 +22,29 @@ def submissions_view():
         current_app.config['API_URL'], 
         'submissions'
     ])
-    response = requests.get(api_url, json=data)
-    print(response.json())
+    response = requests.get(api_url, json=payload)
     return render_template('submissions.html')
 
 @main.route('/submissions', methods=['POST'])
 @login_required
 def submissions_post():
     return redirect(url_for('main.submissions_view'))
+
+@main.route('/image/<int:instance>/<int:id>', methods=['GET'])
+@login_required
+def image(instance, id):
+    payload = {
+        'kobo_username': current_app.config['KOBO_USERNAME'],
+        'kobo_password': current_app.config['KOBO_PASSWORD'],
+        'kobo_uid': current_app.config['KOBO_UID'],
+        'instance': instance,
+        'id': id
+    }
+    api_url = '/'.join([
+        current_app.config['API_URL'],
+        'image'
+    ])
+    image = requests.get(api_url, json=payload)
+    response = make_response(image.content)
+    response.headers.set('Content-Type', 'image/jpeg')
+    return response
