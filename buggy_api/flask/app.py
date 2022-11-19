bffb9e23e4a00b3f0f22c27b54d23c5bbab8efd3
erebuseternal <marcelsanders96@gmail.com>
2022-11-19 14:08:15 -0500

    image

--- buggy_api/flask/app.py
+++ buggy_api/flask/app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_restful import Api
 
 from resources.submissions import Submissions
+from resources.image import Image
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Submissions, '/submissions')
+api.add_resource(Image, '/image')
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=5000)
