299e76bd4465663c47cb5979eabdcdc394e153fb
erebuseternal <marcelsanders96@gmail.com>
2022-11-28 09:03:19 -0800

    network load balancer

--- buggy_api/stacks/service/stack.py
+++ buggy_api/stacks/service/stack.py
@@ -1,5 +1,8 @@
+from sys import intern
 from aws_cdk import (
     aws_ecs as ecs,
+    aws_elasticloadbalancingv2_targets as targets,
+    aws_elasticloadbalancingv2 as elb,
     aws_ec2 as ec2,
     aws_ecr as ecr,
     aws_ecs_patterns as patterns,
@@ -12,8 +15,13 @@ class ServiceStack(Stack):
     def __init__(self, scope: Construct, id: str, conf: dict, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
+        vpc = ec2.Vpc.from_lookup(
+            self, '-'.join([conf['name'], 'vpc']),
+            is_default=True
+        )
+
         service_name = '-'.join([conf['name'], 'service'])
-        patterns.ApplicationLoadBalancedFargateService(
+        service = patterns.ApplicationLoadBalancedFargateService(
             self, service_name,
             memory_limit_mib=512, cpu=256, desired_count=1,
             task_image_options=patterns.ApplicationLoadBalancedTaskImageOptions(
@@ -27,5 +35,20 @@ class ServiceStack(Stack):
                 container_port=5002
             ),
             load_balancer_name=service_name,
-            assign_public_ip=True
+            assign_public_ip=True,
+            vpc=vpc
         )
+
+        # in order to have a static ip we need to put this
+        # behind a network load balancer
+
+        nlb_name = '-'.join([conf['name'], 'network-load-balancer'])
+        nlb = elb.NetworkLoadBalancer(
+            self, nlb_name, load_balancer_name=nlb_name,
+            vpc=vpc, internet_facing=True, cross_zone_enabled=True
+        )
+
+        listener = nlb.add_listener('http-listener', port=80)
+
+        target = targets.AlbTarget(service.load_balancer, 80)
+        listener.add_targets('http-target', targets=[target], port=80)
