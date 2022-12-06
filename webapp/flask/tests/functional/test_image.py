cfd918cd87938e6eec81d034dfb984866a56d5b9
erebuseternal <marcelsanders96@gmail.com>
2022-12-06 09:39:35 -0500

    index and image tests

---
+++ webapp/flask/tests/functional/test_image.py
@@ -0,0 +1,37 @@
+import httpretty
+import json
+
+@httpretty.activate
+def test_w_login(client):
+    with client.session_transaction() as sesh:
+        sesh['_user_id'] = 'dragon@bug.org sixlegsisbest'
+
+    httpretty.register_uri(
+        httpretty.GET, 'http://localhost:5002/image',
+        body=b'flowerpower'
+    )
+
+    response = client.get('/image/32/4')
+    assert response.data == b'flowerpower'
+    assert response.status_code == 200
+    assert response.headers[0] == ('Content-Type', 'image/jpeg')
+    assert response.headers[2] == ('Cache-Control', 'private, max-age=7200')
+    assert json.loads(httpretty.last_request().body.decode('utf-8')) == {
+        'kobo_username': 'beetlebub', 'kobo_password': 'chitinisking',
+        'kobo_uid': 'iguessiamauid', 'instance': 32, 'id': 4
+    }
+
+def test_wo_login(client):
+    response = client.get('/image/32/4', follow_redirects=True)
+    assert response.history[0].status_code == 302
+    assert response.status_code == 200
+    assert response.request.path == '/login'
+    assert response.request.query_string.decode('utf-8') == 'next=%2Fimage%2F32%2F4'
+
+def test_wo_api(client):
+    with client.session_transaction() as sesh:
+        sesh['_user_id'] = 'dragon@bug.org sixlegsisbest'
+
+    response = client.get('/image/32/4')
+    assert response.status_code == 500
+    
